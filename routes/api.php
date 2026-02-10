82a6c756fec22bce2529a4f6d2b125d09f78930e
Rapalexism <rapalexism@gmail.com>
2026-02-09 19:11:48 -0500

    Fix rename to name en rutas api, agregar roles y admins

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,4 @@ Route::get('/user', function (Request $request) {
 
 #Rutas sin auth
 Route::get('/index', [Home::class, 'index']);
-Route::get('/auth', [Home::class, 'auth'])->rename('login');
+Route::get('/auth', [Home::class, 'auth'])->name('login');
