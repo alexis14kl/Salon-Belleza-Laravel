db2c6326b96daa9caa320773754f0ba6678d1825
Rapalexism <rapalexism@gmail.com>
2026-02-09 19:32:13 -0500

    login v0

--- app/Services/HomeService.php
+++ app/Services/HomeService.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Services;
 
+use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isEmpty;
+
 class HomeService {
     // verificar funccionalidad del api
     public function index(){
@@ -8,7 +12,19 @@ class HomeService {
     }
 
     // acceso a la plataforma
-    public function auth(){
+    public function auth(Request $data){
+        $name_email = $data->input('name_email');
+        $password = $data->input('password');
+
+        if(isEmpty($name_email)){
+            return "Usuario o Correo Obligatorio";
+        }
+
+        
+        if(isEmpty($password)){
+            return "Contraseña Obligatoria";
+        }
+        
         return "login ok";
     }
 }
